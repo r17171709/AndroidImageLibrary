bcf9df138a71634c6cd11db797f86329099b20b4
renyu <Yuren2@hotmail.com>
2017-06-06 15:57:55 +0800

    示例微调

--- app/src/main/java/com/renyu/androidimagelibrary/MainActivity.java
+++ app/src/main/java/com/renyu/androidimagelibrary/MainActivity.java
@@ -3,11 +3,17 @@ package com.renyu.androidimagelibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.renyu.imagelibrary.commonutils.Utils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        com.blankj.utilcode.util.Utils.init(this);
+
+        Utils.takePicture(this, 1000);
     }
 }
