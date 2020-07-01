66bdbcb7c2afcbd9df4adbd9f244532c864b1381
renyu <Yuren2@hotmail.com>
2020-07-01 10:36:18 +0800

    视频预览调整分辨率

--- ImageLibrary/src/main/java/com/renyu/imagelibrary/camera/ViedoRecordView.java
+++ ImageLibrary/src/main/java/com/renyu/imagelibrary/camera/ViedoRecordView.java
@@ -9,10 +9,10 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import com.renyu.commonlibrary.params.InitParams;
+import com.renyu.imagelibrary.commonutils.Utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class ViedoRecordView extends SurfaceView implements MediaRecorder.OnErrorListener {
     private SurfaceHolder mSurfaceHolder;
@@ -21,7 +21,8 @@ public class ViedoRecordView extends SurfaceView implements MediaRecorder.OnErro
 
     private String mOutputDirPath = InitParams.IMAGE_PATH;
 
-    private File mVecordFile = null;// 视频输出文件
+    // 视频输出文件
+    private File mVecordFile = null;
 
     public ViedoRecordView(Context context) {
         this(context, null);
@@ -88,56 +89,13 @@ public class ViedoRecordView extends SurfaceView implements MediaRecorder.OnErro
     private void setCameraParams() {
         if (mCamera != null) {
             Camera.Parameters parameters = mCamera.getParameters();
-            Camera.Size preSize = getCloselyPreSize(true, getWidth(), getHeight(), parameters.getSupportedPreviewSizes());
+            Camera.Size preSize = Utils.getCurrentScreenSize(parameters.getSupportedPreviewSizes());
             parameters.setPreviewSize(preSize.width, preSize.height);
             if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
                 parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
             }
             mCamera.setParameters(parameters);
         }
-    }
-
-    /**
-     * 通过对比得到与宽高比最接近的预览尺寸（如果有相同尺寸，优先选择）
-     *
-     * @param isPortrait    是否竖屏
-     * @param surfaceWidth  需要被进行对比的原宽
-     * @param surfaceHeight 需要被进行对比的原高
-     * @param preSizeList   需要对比的预览尺寸列表
-     * @return 得到与原宽高比例最接近的尺寸
-     */
-    public static Camera.Size getCloselyPreSize(boolean isPortrait, int surfaceWidth, int surfaceHeight, List<Camera.Size> preSizeList) {
-        int reqTmpWidth;
-        int reqTmpHeight;
-        // 当屏幕为垂直的时候需要把宽高值进行调换，保证宽大于高
-        if (isPortrait) {
-            reqTmpWidth = surfaceHeight;
-            reqTmpHeight = surfaceWidth;
-        } else {
-            reqTmpWidth = surfaceWidth;
-            reqTmpHeight = surfaceHeight;
-        }
-        //先查找preview中是否存在与surfaceview相同宽高的尺寸
-        for (Camera.Size size : preSizeList) {
-            if ((size.width == reqTmpWidth) && (size.height == reqTmpHeight)) {
-                return size;
-            }
-        }
-
-        // 得到与传入的宽高比最接近的size
-        float reqRatio = ((float) reqTmpWidth) / reqTmpHeight;
-        float curRatio, deltaRatio;
-        float deltaRatioMin = Float.MAX_VALUE;
-        Camera.Size retSize = null;
-        for (Camera.Size size : preSizeList) {
-            curRatio = ((float) size.width) / size.height;
-            deltaRatio = Math.abs(reqRatio - curRatio);
-            if (deltaRatio < deltaRatioMin) {
-                deltaRatioMin = deltaRatio;
-                retSize = size;
-            }
-        }
-        return retSize;
     }
 
     /**
